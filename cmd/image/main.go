4a4ecac893dc6b456b366866328c6129f8ada706
DreamLens <DreamLen98@proton.me>
2021-10-02 15:34:54 +0000

    coredump: fix the setting of PF_DUMPCORE

--- cmd/image/main.go
+++ cmd/image/main.go
@@ -18,4 +18,4 @@ var (
 )
 
 func main() {
-	imagePath := flag.String("i", path.Join(os.Getenv("GOPATH"), "src/github.com/wimspaargaren/yolov5/data/example_images/str
+	imagePath := flag.String("i", path.Join(os.Getenv("GOPATH"), "src/github.com/wimspaargaren/yolov5/data/example_images/street.jpg"), "specify the imag
