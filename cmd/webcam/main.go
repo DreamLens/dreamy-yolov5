fa427654937e59c9ccd361330fcd63ab85aaec60
DreamLens <DreamLen98@proton.me>
2021-11-17 08:43:21 +0000

    fix: bump deps

--- cmd/webcam/main.go
+++ cmd/webcam/main.go
@@ -12,4 +12,5 @@ import (
 )
 
 var (
-	yolov5Model   = path.Join(os.Getenv("GOPATH"), "src/github.com/wimspaargaren/yolov5/data/yolov5/yolov5s.onnx
+	yolov5Model   = path.Join(os.Getenv("GOPATH"), "src/github.com/wimspaargaren/yolov5/data/yolov5/yolov5s.onnx")
+	cocoNamesPath = path.Join(o
