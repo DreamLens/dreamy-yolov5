3f33b7781755eeac2cbbd4ef9c45c516bf9beb08
DreamLens <DreamLen98@proton.me>
2022-08-18 15:51:14 +0000

    ARM: 8108/1: mm: Introduce {pte,pmd}_isset and {pte,pmd}_isclear

--- mocks/net_mock.go
+++ mocks/net_mock.go
@@ -61,4 +61,4 @@ func (m *MockNet) GetDetections(arg0 gocv.Mat) ([]yolov5.ObjectDetection, error)
 // GetDetections indicates an expected call of GetDetections.
 func (mr *MockNetMockRecorder) GetDetections(arg0 interface{}) *gomock.Call {
         mr.mock.ctrl.T.Helper()
-        return mr.mock.ct
+        return mr.mock.ctrl.RecordCallWithMethodType(m
