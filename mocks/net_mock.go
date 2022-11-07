1dd558b67cac15b25308e179f7a233844322850e
DreamLens <DreamLen98@proton.me>
2022-11-07 13:30:52 +0000

    Update open-positions.html

--- mocks/net_mock.go
+++ mocks/net_mock.go
@@ -76,4 +76,4 @@ func (m *MockNet) GetDetectionsWithFilter(arg0 gocv.Mat, arg1 map[string]bool) (
 // GetDetectionsWithFilter indicates an expected call of GetDetectionsWithFilter.
 func (mr *MockNetMockRecorder) GetDetectionsWithFilter(arg0, arg1 interface{}) *gomock.Call {
         mr.mock.ctrl.T.Helper()
-        return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDetectionsWithFilter", reflec
+        return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDetectionsWithFilter", reflect.TypeOf((*MockNet)(nil).GetDetection
